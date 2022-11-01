365da42e4deeb34e378a1107b01c79dc664778ca
unknown <ptmarmello@gmail.com>
2022-11-01 17:05:23 -0300

    manage.py almost done

--- stockproject/manage.py
+++ stockproject/manage.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+from ast import IsNot
+from contextlib import nullcontext
 import os
 import sys
-
+from stockproject import views
 
 def main():
+    if len(sys.argv[1])> 5 and sys.argv[2]:
+        stock = sys.argv[2]
+        firstValue = sys.argv[3]
+        secondValue = sys.argv[4]
+        server = True
+    elif len(sys.argv[4]) > 5 and sys.argv[2]:
+        stock = sys.argv[1]
+        firstValue = sys.argv[2]
+        secondValue = sys.argv[3]
+        server = True
+    elif sys.argv[2]:
+        stock = sys.argv[1]
+        firstValue = sys.argv[2]
+        secondValue = sys.argv[3]
+        server = False
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockproject.settings')
     try:
@@ -15,7 +32,21 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    
+    if stock is None or firstValue is None or secondValue is None:
+        raise Exception(
+            "                                                            "
+            "                                                            "
+            "Sorry, you must insert a Stock symbol like 'PETR4' "
+            "and valid values for stock monitoring, such as: 20.9 30.6 "
+            "Why don't you give it a try again?"
+            "execute 'python manage.py petr4 19.5 27.9' "
+            "or 'python manage.py petr4 27.9 19.5 runserver' to test each function by route"
+        )
+
+    views.Test(stock, firstValue, secondValue)
+    if server:
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
--- stockproject/stockproject/views.py
+++ stockproject/stockproject/views.py
@@ -10,13 +10,14 @@ from mailsender import views
 
 apiKey = config('STOCK_API_KEY')
 
-def Test(apiKey):
-    stockSymbolTest = input("Qual ação você deseja?").upper()
-    firstValue = float( input("Qual o seu primeiro valor?"))
-    secondValue = float( input("Qual o segundo valor?"))
-    value = 29.6
+def Test(stockSymbol, firstValue, secondValue ):
+    stockSymbol = stockSymbol.upper()
+    print( stockSymbol, firstValue, secondValue )
+    firstValue = float(firstValue)
+    secondValue = float(secondValue)
+
     try:
-        response = requests.get('https://api.twelvedata.com/time_series?apikey={}&interval=1min&symbol={}&format=JSON&outputsize=1')
+        response = requests.get('https://api.twelvedata.com/time_series?apikey=03f25006a3764aaa84bd1d1843a7c915&interval=1min&symbol=petr4&format=JSON&outputsize=1')
     except ImportError as exc:
         raise ImportError(
             "Couldn't request the api. Are you sure it's the right apiKey? "
@@ -24,10 +25,11 @@ def Test(apiKey):
             "does it appear on our Stock whitelist?"
             "If the error persists, get in touch with your administrator"
         ) from exc
+    
     if response:
         data = response.json()
         value = float(data['values'][0]['close'])
-        # print( value )
+        print("Live Stock Value: " + value )
     else:
         return
         
@@ -38,10 +40,12 @@ def Test(apiKey):
     """
     emailType = ApiRequest.ValueComp(firstValue=firstValue, secondValue=secondValue, respValue=value)
     
-    # if emailType == "Continua" return to loop begginning
-    views.mailtester(emailtype=emailType)
-
-    return HttpResponse('Oh Hello, Peter ')
+    if emailType == "Continua":
+        return 
+    else:
+        views.mailtester(emailtype=emailType)
+    
+    return
 
 
 
@@ -53,12 +57,10 @@ class ApiRequest():
             raise ImportError(
                 "Couldn't request the api. Are you sure it's the right apiKey? "
                 "Perhaps you should check your Stock name as well "
-                "does it appear on our Stock whitelist?"
+                "Does it appear on our Stock whitelist?"
                 "If the error persists, get in touch with your administrator"
             ) from exc
         if response:
-            print(response)
-            HttpResponse(response)
             emailtype = ApiRequest.ValueComp(firstValue=firstValue, secondValue=secondValue, respValue=response.json())
             views.MailSender.mailsend(emailtype=emailtype)
         return
